f877f4eeb89e0a2bc90027e9121f0b385832a1e3
shaheermudassar <124002206+shaheermudassar@users.noreply.github.com>
2024-04-01 17:39:56 +0500

    Add files via upload

---
+++ Lecture 5 (Admin Panel Customization)/Recipe_Realm/userauths/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from userauths.models import Profile
+
+admin.site.register(Profile)
+
---
+++ Lecture 5 (Admin Panel Customization)/Recipe_Realm/userauths/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from userauths import views
+urlpatterns = [
+    path("login",views.login_view, name="login"),
+    path("signup",views.signup_view, name="signup"),
+    path("logout",views.logout_view, name="logout"),
+]
